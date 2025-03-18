ffe2439c4ff4096be39bb82e7c88fd178ad6f733
Anton-maxi <zborikanton@gmail.com>
2025-03-18 22:26:32 +0200

    Laboratory2

--- Laboratory2/laboratory2.c
+++ Laboratory2/laboratory2.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 int main() {
+int numberElements, i,n;
+printf("Enter the number of elements: ");
+scanf("%d", &numberElements);
+float A[numberElements];
+printf("Enter the values of the array elements:\n");
+for(i=0;i<numberElements; i++){
+scanf("%f", &A[i]);
+}
+float min[numberElements];
+int r; 
+for(i=0;i<numberElements; i++){
+    r=(numberElements-1)-i;
+        min[i]=fabs(A[r]-A[i]);
+        if(r<=i){
+            break;
+        }
+}
 
+printf("Result:\n");
+for (i = 0; i < numberElements; i++) {
+    if (i >= (numberElements + 1) / 2) {
+        break; 
+    }
+    printf("%4.2f\n", min[i]);
+}
 return 0;
 
 }
